954d6dc75e0b05dd21ede5f1064b64ad61140aa5
Yempada Lokesh <106131220+Lokesh1044@users.noreply.github.com>
2023-01-09 19:20:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,22 +26,22 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-     model:env.model,
+     model:process.env.model,
       prompt: `${prompt}`,
       // temperature: 0, 
       // max_tokens: 3000, 
       // top_p: 1, 
       // frequency_penalty: 0.5, 
       // presence_penalty: 0,       
-      temperature: env.temperature, 
+      temperature:process.env.temperature, 
       // Higher values means the model will take more risks.
-      max_tokens: env.max_tokens, 
+      max_tokens: process.env.max_tokens, 
       // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: env.top_p, 
+      top_p: process.env.top_p, 
       // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: env.frequency_penalty, 
+      frequency_penalty: process.env.frequency_penalty, 
       // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: env.presence_penalty,
+      presence_penalty: process.env.presence_penalty,
        // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
      });
 
