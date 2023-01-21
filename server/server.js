7524638abe0af9577f09ee91ce0162b6b96f6609
Yempada Lokesh <106131220+Lokesh1044@users.noreply.github.com>
2023-01-21 16:00:13 +0530

    Update server.js
    iiiiiiiiiiiiiiiiiii

--- server/server.js
+++ server/server.js
@@ -33,11 +33,11 @@ app.post('/', async (req, res) => {
       // top_p: 1, 
       // frequency_penalty: 0.5, 
       // presence_penalty: 0,       
-      temperature: 0, 
-      max_tokens: 3000, 
-      top_p: 1, 
-      frequency_penalty: 0.5, 
-      presence_penalty: 0,  
+      temperature: process.env.temperature, 
+      max_tokens: process.env.max_tokens, 
+      top_p: parseFloat(process.env.top_p),
+       frequency_penalty: parseFloat(process.env.frequency_penalty),
+       presence_penalty: parseFloat(process.env.presence_penalty)
     });
 
     res.status(200).send({
